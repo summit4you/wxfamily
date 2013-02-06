2109eef1125d1599396246f3c20c739e4c2cf078
summit <summit.qiu@gmail.com>
2013-02-06 14:17:21 +0800

    init

--- source/wx_detail.php
+++ source/wx_detail.php
@@ -3,7 +3,7 @@
 if (isset($_COOKIE['uchome_m_auth'])) 
 {
  	$m_auth=$_COOKIE['uchome_m_auth'];
- 	$wxkey=$_COOKIE['uchome_wxkey'];
+ 	$wxkey=$_GET['wxkey'];
 }else{
 	showmessage('login_failure_please_re_login',  'wx.php?do=bind&wxkey='.$_GET['wxkey']);
 }
