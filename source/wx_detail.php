192c948de0045b96e8d4b0cbcdd8cd91bc2e04b5
summit <summit.qiu@gmail.com>
2013-02-07 02:01:36 +0800

    init

--- source/wx_detail.php
+++ source/wx_detail.php
@@ -5,6 +5,11 @@ if (isset($_COOKIE['uchome_m_auth']))
  	$m_auth=$_COOKIE['uchome_m_auth'];
  	$wxkey=$_GET['wxkey'];
 }else{
+	$query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('space')." WHERE wxkey='$_GET[wxkey]'");
+
+	if ($value=$_SGLOBAL['db']->fetch_array($query)){
+		updatetable('space', array('wxkey'=>''), array('uid'=>$value['uid']));
+	}
 	showmessage('login_failure_please_re_login',  'wx.php?do=bind&wxkey='.$_GET['wxkey']);
 }
 
