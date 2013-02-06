fc1bb8d599097d5c426a414c38a24a3f90c4810a
summit <summit.qiu@gmail.com>
2013-02-06 17:01:31 +0800

    init

---
+++ source/wx_cp.php
@@ -0,0 +1,12 @@
+<?php
+
+if (isset($_COOKIE['uchome_m_auth'])) 
+{
+ 	$m_auth=$_COOKIE['uchome_m_auth'];
+ 	$wxkey=$_GET['wxkey'];
+}else{
+	showmessage('login_failure_please_re_login',  'wx.php?do=bind&wxkey='.$_GET['wxkey']);
+}
+
+include_once template("./wx/template/cp");
+?>
