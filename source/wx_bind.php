5668875fe5c6b928f4dc282503afa357cb1dc362
summit <summit.qiu@gmail.com>
2013-02-07 17:43:03 +0800

    init

--- source/wx_bind.php
+++ source/wx_bind.php
@@ -42,12 +42,12 @@ if ($_GET["op"]=="add"){
 	updatetable('space', array('wxkey'=>$_POST['wxkey']), array('uid'=>$passport['uid']));
 
 	// 同步登陆
-	$jsonurl = "http://www.familyday.com.cn/dapi/do.php?ac=login&iscookie=1&username=".$_POST['username']."&password=".$_POST['password'];
+	$jsonurl = "http://www.familyday.com.cn/dapi/do.php?ac=login&username=".$username."&password=".$password;
 	$json = file_get_contents($jsonurl,0,null,null);
 	$json_output = json_decode($json);
 	
-	echo "<script>localStorage.setItem('auth','".$json_output->data->m_auth."');</script>";
-	echo "<script>localStorage.setItem('wxkey','".$_POST['wxkey']."');</script>";
+	echo "<script>localStorage.removeItem('auth');localStorage.setItem('auth','".$json_output->data->m_auth."');</script>";
+	
 
 	
 
