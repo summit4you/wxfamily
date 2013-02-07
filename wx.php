466b6794925bf7baeebb4a8a30f22e488163d1fe
summit <summit.qiu@gmail.com>
2013-02-07 21:22:38 +0800

    init

--- wx.php
+++ wx.php
@@ -4,7 +4,7 @@ include_once('./../common.php');
 include_once( 'botutil.php' );
 include_once( 'wx_common.php' );
 //ÔÊÐí¶¯×÷
-$dos = array('bind', 'feed', 'cp', 'detail', 'reg', 'setting');
+$dos = array('bind', 'feed', 'cp', 'detail', 'reg', 'setting', 'invite');
 $do = (!empty($_GET['do']) && in_array($_GET['do'], $dos))?$_GET['do']:'bind';
 
 include_once(S_ROOT."./wx/source/wx_{$do}.php");
