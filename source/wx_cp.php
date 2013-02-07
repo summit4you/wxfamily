10adaf9e758e14303341a18036bb53177f9d72bf
summit <summit.qiu@gmail.com>
2013-02-07 10:53:46 +0800

    init

--- source/wx_cp.php
+++ source/wx_cp.php
@@ -13,19 +13,20 @@ if (isset($_COOKIE['uchome_m_auth']))
 	showmessage('login_failure_please_re_login',  'wx.php?do=bind&wxkey='.$_GET['wxkey']);
 }
 
-if ($_GET[op]=="photo"){
+
 	
-	$taglist = array();
+$taglist = array();
 
-	$query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('space')." WHERE wxkey='$_GET[wxkey]'");
+$query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('space')." WHERE wxkey='$_GET[wxkey]'");
 
-	if ($space=$_SGLOBAL['db']->fetch_array($query)){
-		$query = $_SGLOBAL['db']->query("select * from ".tname('tag')." where close = 0 and uid='$space[uid]'");
-		while($value = $_SGLOBAL['db']->fetch_array($query)){
-			$taglist[] = $value;
-		}
+if ($space=$_SGLOBAL['db']->fetch_array($query)){
+	$query = $_SGLOBAL['db']->query("select * from ".tname('tag')." where close = 0 and uid='$space[uid]'");
+	while($value = $_SGLOBAL['db']->fetch_array($query)){
+		$taglist[] = $value;
 	}
 }
+
+$op = empty($_GET[op])?'photo':$_GET[op];
 
 
 include_once template("./wx/template/cp");
