11faef400864564c6f652d72b9944c875cb2f35f
summit <summit.qiu@gmail.com>
2013-02-07 08:53:41 +0800

    init

--- source/wx_detail.php
+++ source/wx_detail.php
@@ -13,5 +13,19 @@ if (isset($_COOKIE['uchome_m_auth']))
 	showmessage('login_failure_please_re_login',  'wx.php?do=bind&wxkey='.$_GET['wxkey']);
 }
 
+if ($_GET[op]=="photo"){
+	
+	$taglist = array();
+
+	$query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('space')." WHERE wxkey='$_GET[wxkey]'");
+
+	if ($space=$_SGLOBAL['db']->fetch_array($query)){
+		$query = $_SGLOBAL['db']->query("select * from ".tname('tag')." where close = 0 and uid='$space[uid]'");
+		while($value = $_SGLOBAL['db']->fetch_array($query)){
+			$taglist[] = $value;
+		}
+	}
+}
+
 include_once template("./wx/template/detail");
 ?>
