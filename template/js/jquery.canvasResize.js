c7ec557a3596bf1c563d704081e1870f9112a5f8
summit <summit.qiu@gmail.com>
2013-02-10 01:34:01 +0800

    init

--- template/js/jquery.canvasResize.js
+++ template/js/jquery.canvasResize.js
@@ -41,7 +41,7 @@
         dataURLtoBlob: function (data){
             var mimeString = data.split(',')[0].split(':')[1].split(';')[0];
             var byteString = atob(data.split(',')[1]);
-            var ab = new ArrayBufferView(byteString.length);
+            var ab = new ArrayBuffer(byteString.length);
             var ia = new Uint8Array(ab);
             for (var i = 0; i < byteString.length; i++) {
                 ia[i] = byteString.charCodeAt(i);
