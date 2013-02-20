02933253b5b78a2bf33420fced90202d45a293a5
summit <summit.qiu@gmail.com>
2013-02-21 00:52:51 +0800

    modify

--- wx.php
+++ wx.php
@@ -6,6 +6,7 @@ include_once( 'wx_common.php' );
 
 define(BLOCK_TAG_START, "{{");
 define(BLOCK_TAG_END, "}}");
+define(DOLLOR_TAG, "$");
 //ÔÊÐí¶¯×÷
 $dos = array('bind', 'feed', 'cp', 'detail', 'reg', 'setting', 'invite');
 $do = (!empty($_GET['do']) && in_array($_GET['do'], $dos))?$_GET['do']:'bind';
