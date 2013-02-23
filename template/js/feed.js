561b58ab2b79b8e55922f3b6b8d6f23f6a5b3458
summit <summit.qiu@gmail.com>
2013-02-23 14:07:53 +0800

    modify

--- template/js/feed.js
+++ template/js/feed.js
@@ -39,6 +39,31 @@ function getfeeds2(auth, perpage, page){
 				}
 				$("#feedTemplate").tmpl(data ).appendTo('#feedlist');
 				$('#page').val(parseInt($('#page').val())+1);
+				$('div#feed-page')
+					.live('pageshow', function(e){
+						
+						var 
+							currentPage = $(e.target),
+							options = {},
+							photoSwipeInstance = $("div.content-image2 a", e.target).photoSwipe(options,  currentPage.attr('id'));
+							
+						return true;
+						
+					})
+					
+					.live('pagehide', function(e){
+						
+						var 
+							currentPage = $(e.target),
+							photoSwipeInstance = PhotoSwipe.getInstance(currentPage.attr('id'));
+
+						if (typeof photoSwipeInstance != "undefined" && photoSwipeInstance != null) {
+							PhotoSwipe.detatch(photoSwipeInstance);
+						}
+						
+						return true;
+						
+					});
 			  }
 		  }else{
 			alert(data.msg);
@@ -110,7 +135,7 @@ $(function(){
 			
 			$(document).ready(function(){
 				
-				$('div.feed-page')
+				$('div#feed-page')
 					.live('pageshow', function(e){
 						
 						var 
